c2e305279546369cfbec42f6c7d280598a0d639d
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-10-31 19:07:41 -0400

    person class and test

---
+++ src/main/java/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+
+    public Person(int id,String firstName,String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,49 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+
+    Person newPerson = new Person(123,"wes","connors");
+
+
+    @Test       // Set and get test
+    public void FirstNameTest(){
+        String expected = "bob";
+        newPerson.setFirstName(expected);
+
+        String actual = newPerson.getFirstName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test       // Set and get test
+    public void LastNameTest(){
+        String expected = "billy";
+        newPerson.setLastName(expected);
+
+        String actual = newPerson.getLastName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test       // Set and get test
+    public void PhoneNumberTest(){
+        String expected = "1234567890";
+        newPerson.setPhoneNumber(expected);
+
+        String actual = newPerson.getPhoneNumber();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test       // Set and get test
+    public void AddressTest(){
+        String expected = "1 Main Street";
+        newPerson.setAddress(expected);
+
+        String actual = newPerson.getAddress();
+
+        Assert.assertEquals(expected,actual);
+    }
+}
