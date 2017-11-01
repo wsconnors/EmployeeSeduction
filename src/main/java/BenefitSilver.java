ba39d36de35a24e62d8caf2009947dc9357d0787
Zachary <zstimmel@gmail.com>
2017-11-01 07:47:36 -0400

    benePackage

---
+++ src/main/java/BenefitSilver.java
@@ -0,0 +1,24 @@
+public class BenefitSilver extends BenefitPackage{
+
+    //lowest benefit package
+
+    private String name = "Silver Package";
+
+    //silver package gets default 15 days / 3 weeks
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public BenefitSilver(){
+
+        super();
+    }
+
+
+
+
+
+}
