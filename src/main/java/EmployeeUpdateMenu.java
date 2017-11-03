838d82af4c505fde678bd0db0e861d6e863be48e
graham <graham@zipcoders-MacBook-Pro.local>
2017-11-03 08:19:38 -0400

    employee update and benefit package update

--- src/main/java/EmployeeUpdateMenu.java
+++ src/main/java/EmployeeUpdateMenu.java
@@ -1,8 +1,12 @@
 public class EmployeeUpdateMenu extends Menu {
     public static final EmployeeUpdateMenu INSTANCE = new EmployeeUpdateMenu();
 
-    enum employeeUpdateEnums {FIRST_NAME, LAST_NAME, ADDRESS, PHONE_NUMBER, SALARY,
-        SALARY_OR_HOURLY, BENEFITS, DEPARTMENT, POSITION, BACK};
+    enum employeeUpdateEnums {
+        FIRST_NAME, LAST_NAME, ADDRESS, PHONE_NUMBER, SALARY,
+        SALARY_OR_HOURLY, BENEFITS, DEPARTMENT, POSITION, BACK
+    }
+
+    ;
 
     private Employee selectedEmployee;
 
@@ -21,7 +25,8 @@ public class EmployeeUpdateMenu extends Menu {
     @Override
     public void userMenuSelection(String input) {
 
-        switch (employeeUpdateEnums.valueOf(input)){
+
+        switch (employeeUpdateEnums.valueOf(input)) {
             case FIRST_NAME:
                 updateFirstName();
                 break;
@@ -57,60 +62,64 @@ public class EmployeeUpdateMenu extends Menu {
     }
 
     public void updateAddress() {
-         this.selectedEmployee.setAddress(Console.getString("Enter new address: "));
+        Console.print("Employee's current address: " + selectedEmployee.getAddress());
+        this.selectedEmployee.setAddress(Console.getString("Enter new address: "));
     }
 
     public void updateLastName() {
+        Console.print("Employee's current last name: " + selectedEmployee.getLastName());
         this.selectedEmployee.setLastName(Console.getString("Enter new last name: "));
     }
 
     public void updateFirstName() {
+        Console.print("Employee's current first name: " + selectedEmployee.getFirstName());
         this.selectedEmployee.setFirstName(Console.getString("Enter new first name: "));
     }
 
-    private void updatePhoneNumber(){
+    private void updatePhoneNumber() {
+        Console.print("Employee's current phone #: " + selectedEmployee.getPhoneNumber());
         this.selectedEmployee.setPhoneNumber(Console.getString("Enter new phone number: "));
     }
 
-    private void updateSalary(){
+    private void updateSalary() {
+        Console.print("Emplyoyee's current salary: " + selectedEmployee.getSalary());
         this.selectedEmployee.setSalary(Console.getDouble("Enter a new salary amount: "));
     }
 
-    private void updateSalaryOrHourly(){
+    private void updateSalaryOrHourly() {
+        Console.print(printIsSalary());
         String input = Console.getString("Salary or Hourly? ");
-        //needs if statement to set true or false
-        if("Salary".equalsIgnoreCase(input)){
+
+        if ("Salary".equalsIgnoreCase(input)) {
             this.selectedEmployee.setIsSalary(true);
-        }
-        else if("Hourly".equalsIgnoreCase(input)){
+        } else if ("Hourly".equalsIgnoreCase(input)) {
             this.selectedEmployee.setIsSalary(false);
-        }
-        else{
-            System.out.println("Error!");
+        } else {
+            Console.print("!!!ERROR!!!");
         }
     }
 
-    private void updateBenefits(){
+    private void updateBenefits() {
+        Console.print("Employee's current benefits package: " + selectedEmployee.getBenefitPackage().printName());
         String input = Console.getString("Enter new benefits package(SILVER/GOLD/PLATINUM): ");
 
-        if("silver".equalsIgnoreCase(input)){
+        if ("silver".equalsIgnoreCase(input)) {
             BenefitSilver silver = new BenefitSilver();
             this.selectedEmployee.setBenefitPackage(silver);
-        }
-        else if("gold".equalsIgnoreCase(input)){
+        } else if ("gold".equalsIgnoreCase(input)) {
             BenefitGold gold = new BenefitGold();
             this.selectedEmployee.setBenefitPackage(gold);
-        }
-        else if("platinum".equalsIgnoreCase(input)){
+        } else if ("platinum".equalsIgnoreCase(input)) {
             BenefitPlat platinum = new BenefitPlat();
             this.selectedEmployee.setBenefitPackage(platinum);
         }
     }
 
-    private void updateDepartment(){
+    private void updateDepartment() {
+        Console.print("Employee's current department: " + selectedEmployee.getDepartment());
         String input = Console.getString("Enter new department: ");
 
-        switch (Department.valueOf(input)){
+        switch (Department.valueOf(input)) {
             case MANAGEMENT:
                 this.selectedEmployee.setDepartment(Department.MANAGEMENT);
                 break;
@@ -121,16 +130,23 @@ public class EmployeeUpdateMenu extends Menu {
                 this.selectedEmployee.setDepartment(Department.FIANCE);
                 break;
             default:
-                System.out.println("Error!");
+               Console.print("!!!ERROR!!!");
         }
     }
 
-    private void updatePosition(){
+    private void updatePosition() {
+        Console.print("Employee's current position: " + selectedEmployee.getDepartment());
         this.selectedEmployee.setPosition(Console.getString("Enter a new position: "));
     }
 
-//    private void mainMenu(){
-//        MainMenu mainMenu = new MainMenu();
-//        MainMenu.display();
-//    }
+    private String printIsSalary(){
+        if(selectedEmployee.getIsSalary()){
+            return "Salary";
+        }
+        else{
+            return "Hourly";
+        }
+    }
+
+
 }
